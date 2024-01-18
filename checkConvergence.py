ec26e38b84e45bd224cd7c7709666487a7ba9e93
Hydroforth <josuico@up.edu.ph>
2024-01-18 23:22:08 +0800

    Generated Position Basis Data
    Wrote a short python script that's supposed to check for convergence and ran this to generate data sets for all position basis Hamiltonians across each ansatz. Will choose data from this to be graphed as part of the final manuscript. Next step is to do the same for the energy basis Hamiltonians.

--- checkConvergence.py
+++ checkConvergence.py
@@ -12,7 +12,7 @@ def multiRuns(runs, H, ansatz, paramnums,fname):
         save = H["IIII"]
         del(H["IIII"])
     for i in range(runs):
-        print("\n_____Multi-Run: Iteration "+str(i)+"_____\n")
+        print("\n_____Multi-Run: Iteration "+str(i+1)+"_____\n")
         results = runVQE(H,ansatz,paramnums)
         multires.append([results[0] + save,results[1]])
     lowvals = [i[0] for i in multires]
@@ -23,4 +23,6 @@ def multiRuns(runs, H, ansatz, paramnums,fname):
 
     return multires
 
-check = multiRuns(1,h2zixy(QHO(4,"pos")),Two_Qubit_HEA,8,"genData/trialrun")
+multiRuns(30,h2zixy(QHO(4,"pos")),Two_Qubit_HEA,8,"genData/2QHEAPosBasis_30runs")
+multiRuns(30,h2zixy(QHO(4,"pos")),Two_Qubit_UniversalAnsatz,16,"genData/2QUAPosBasis_30runs")
+multiRuns(30,h2zixy(QHO(16,"pos")),Four_Qubit_HEA,16,"genData/4QHEQPosBasis_30runs")
